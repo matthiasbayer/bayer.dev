e515be15967533e84a1a7a30e22bf71c6c4766b8
Matthias Bayer <thematthiasbayer@gmail.com>
2023-06-09 11:30:49 +0200

    wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,31 @@
-import "./globals.css";
-import { Montserrat } from "next/font/google";
-import clsx from "clsx";
+import './globals.css'
+import { Montserrat } from 'next/font/google'
+import clsx from 'clsx'
+import { Analytics } from '@vercel/analytics/react'
 
-const montserrat = Montserrat({ subsets: ["latin"] });
+const montserrat = Montserrat({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "Matthias Bayer",
-  description: "Software Engineer",
-};
+  title: 'Matthias Bayer',
+  description: 'Software Engineer',
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
       <body
         className={clsx(
           montserrat.className,
-          "antialiased bg-white dark:bg-neutral-950 text-neutral-800 dark:text-white"
+          'antialiased bg-white dark:bg-neutral-950 text-neutral-800 dark:text-white'
         )}
       >
         {children}
+        <Analytics />
       </body>
     </html>
-  );
+  )
 }
