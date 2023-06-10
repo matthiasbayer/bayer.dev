3c05a1667a72112874cb2dd1e96a767df72e4377
Matthias Bayer <thematthiasbayer@gmail.com>
2023-06-11 01:08:09 +0200

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,6 @@ export default function Home() {
                 href="https://github.com/matthiasbayer"
                 target="_blank"
                 rel="noopener noreferer"
-                className="underline decoration-blue-600 underline-offset-4"
               >
                 <svg
                   role="img"
@@ -63,7 +62,6 @@ export default function Home() {
                 href="https://twitter.com/matthias_bayer"
                 target="_blank"
                 rel="noopener noreferer"
-                className="underline decoration-blue-600 underline-offset-4"
               >
                 <svg
                   role="img"
@@ -80,7 +78,6 @@ export default function Home() {
                 href="https://linkedin.com/in/matthias-bayer/"
                 target="_blank"
                 rel="noopener noreferer"
-                className="underline decoration-blue-600 underline-offset-4"
               >
                 <svg
                   role="img"
@@ -90,6 +87,18 @@ export default function Home() {
                 >
                   <title>LinkedIn</title>
                   <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
+                </svg>
+              </a>
+
+              <a href="mailto:hello@bayer.dev">
+                <svg
+                  role="img"
+                  viewBox="0 0 24 24"
+                  className="w-5 h-5 fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <title>Email</title>
+                  <path d="M15.61 12c0 1.99-1.62 3.61-3.61 3.61-1.99 0-3.61-1.62-3.61-3.61 0-1.99 1.62-3.61 3.61-3.61 1.99 0 3.61 1.62 3.61 3.61M12 0C5.383 0 0 5.383 0 12s5.383 12 12 12c2.424 0 4.761-.722 6.76-2.087l.034-.024-1.617-1.879-.027.017A9.494 9.494 0 0 1 12 21.54c-5.26 0-9.54-4.28-9.54-9.54 0-5.26 4.28-9.54 9.54-9.54 5.26 0 9.54 4.28 9.54 9.54a9.63 9.63 0 0 1-.225 2.05c-.301 1.239-1.169 1.618-1.82 1.568-.654-.053-1.42-.52-1.426-1.661V12A6.076 6.076 0 0 0 12 5.93 6.076 6.076 0 0 0 5.93 12 6.076 6.076 0 0 0 12 18.07a6.02 6.02 0 0 0 4.3-1.792 3.9 3.9 0 0 0 3.32 1.805c.874 0 1.74-.292 2.437-.821.719-.547 1.256-1.336 1.553-2.285.047-.154.135-.504.135-.507l.002-.013c.175-.76.253-1.52.253-2.457 0-6.617-5.383-12-12-12" />
                 </svg>
               </a>
             </div>
